0f3fad930a4e135c35571cd5270d9be6709fe676
Adam Camacho <adamcamacho8@gmail.com>
2016-12-16 23:05:00 -0600

    added email functionality but removed email for now

--- index.php
+++ index.php
@@ -8,14 +8,61 @@ date_default_timezone_set("America/Mexico_City");
 //$log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
 //$log->addWarning('Oh Noes.');
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim( array(
+     'view' => new \Slim\Views\Twig()
+));
 
-$app->get('/', function(){
-  echo 'Hello, this is the home page.';
-});
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true
+);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
 
-$app->get('/contact', function() {
-  echo 'Feel free to contact us.';
+$app->get('/', function() use($app){
+  $app->render('about.twig');
+})->name('home');
+
+$app->get('/contact', function() use($app) {
+  $app->render('contact.twig');
+})->name('contact');
+
+$app->post('/contact', function() use($app) {
+  $name = $app->request->post('name');
+  $email = $app->request->post('email');
+  $msg = $app->request->post('msg');
+  if (!empty($name) && !empty($email) && !empty($msg)) {
+      $cleanName = filter_var($name, FILTER_SANITIZE_STRING);
+      $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+      $cleanMsg = filter_var($msg, FILTER_SANITIZE_STRING);
+  }   else {
+     //message the user there was a problem
+     $app->redirect('/contact');
+  }
+
+  $transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+  $mailer = \Swift_Mailer::newInstance($transport);
+
+  $message = \Swift_Message::newInstance();
+  $message->setSubject('Email From Our Website');
+  $message->setFrom(array(
+          $cleanEmail => $cleanName
+ ));
+ $message->setTo(array('testaccountthatdoesnotexist@emailprovider.com'));
+ $message->setBody($cleanMsg);
+
+ $result = $mailer->send($message);
+
+ if($result > 0 ) {
+     // send a message that says thank you.
+     $app->redirect('/');
+} else {
+     // send a message to the user that the message failed to send
+     // log that there was an error
+     $app->redirect('/contact');
+}
+
 });
 
 $app->run();
